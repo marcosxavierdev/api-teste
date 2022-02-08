184f0ea5f978274442a0a8f5becc08b6fac2730a
marcosxavierdev <sis.marcosxavier@gmail.com>
2022-02-08 16:44:25 -0300

    classe UserServiceImplementation e metodo findById criados

---
+++ src/main/java/br/com/marcosxavierdev/apitestes/service/implementation/UserServiceImplementation.java
@@ -0,0 +1,22 @@
+package br.com.marcosxavierdev.apitestes.service.implementation;
+
+import br.com.marcosxavierdev.apitestes.domain.User;
+import br.com.marcosxavierdev.apitestes.repositories.UserRepository;
+import br.com.marcosxavierdev.apitestes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImplementation implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
