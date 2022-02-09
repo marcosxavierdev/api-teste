c323f039df6b734295f26d2135d177a25ccfb031
marcosxavierdev <sis.marcosxavier@gmail.com>
2022-02-09 14:19:17 -0300

    classe UserDTO criada

---
+++ src/main/java/br/com/marcosxavierdev/apitestes/domain/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package br.com.marcosxavierdev.apitestes.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+}
