4ede367b3d061d99e15cee9d4df1437ba1c7690a
marcosxavierdev <sis.marcosxavier@gmail.com>
2022-02-09 16:45:05 -0300

    delete criado

--- src/main/java/br/com/marcosxavierdev/apitestes/resources/UserResource.java
+++ src/main/java/br/com/marcosxavierdev/apitestes/resources/UserResource.java
@@ -17,13 +17,15 @@ import java.util.stream.Collectors;
 @RequestMapping(value="/user")
 public class UserResource {
 
+    public static final String ID = "/{id}";
+
     @Autowired
     private ModelMapper mapper;
 
     @Autowired
     private UserService userService;
 
-    @GetMapping(value="/{id}")
+    @GetMapping(value=ID)
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
         return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
     }
@@ -42,10 +44,16 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping(value="/{id}")
+    @PutMapping(value=ID)
     public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj){
         obj.setId(id);
         return ResponseEntity.ok().body(mapper.map(userService.update(obj), UserDTO.class));
+    }
+
+    @DeleteMapping(value= ID)
+    public ResponseEntity<UserDTO> delete (@PathVariable Integer id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
