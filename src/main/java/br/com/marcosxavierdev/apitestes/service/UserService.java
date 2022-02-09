df62e6d1d73a451c5385f0ac6812929deafa77fe
marcosxavierdev <sis.marcosxavier@gmail.com>
2022-02-09 14:54:56 -0300

    findAll criado

--- src/main/java/br/com/marcosxavierdev/apitestes/service/UserService.java
+++ src/main/java/br/com/marcosxavierdev/apitestes/service/UserService.java
@@ -2,7 +2,10 @@ package br.com.marcosxavierdev.apitestes.service;
 
 import br.com.marcosxavierdev.apitestes.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById (Integer id);
+    List<User> findAll();
 }
