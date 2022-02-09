1bd1f3661d02cbf2a08eeec4cd2909ef501b47d5
marcosxavierdev <sis.marcosxavier@gmail.com>
2022-02-09 14:13:29 -0300

    tratamento de exception para objeto nao encontrado

--- src/main/java/br/com/marcosxavierdev/apitestes/service/implementation/UserServiceImplementation.java
+++ src/main/java/br/com/marcosxavierdev/apitestes/service/implementation/UserServiceImplementation.java
@@ -3,6 +3,7 @@ package br.com.marcosxavierdev.apitestes.service.implementation;
 import br.com.marcosxavierdev.apitestes.domain.User;
 import br.com.marcosxavierdev.apitestes.repositories.UserRepository;
 import br.com.marcosxavierdev.apitestes.service.UserService;
+import br.com.marcosxavierdev.apitestes.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImplementation implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
